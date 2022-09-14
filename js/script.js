8747d753e6df11ce2e00208cf8fc67e3c5897964
khabibullokhon <k01346595@gmail.com>
2022-09-14 20:20:03 +0500

    js 70% done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
-const form = document.getElementById("header__form")
 const input = document.getElementById("header__input")
 const addButton = document.getElementById("header__add-btn")
 
-addButton.addEventListener("click", function (e) {
-    e.preventDefault()
-    return alert(input.value)
+addButton.addEventListener("click", function () {
+
+    if(input.value.trim() != 0){
+        let localItems = 
+        JSON.parse(localStorage.getItem('localItem'))
+
+        if (localItems === null) {
+            taskList = []
+        } else{
+            taskList = localItems
+        }
+        taskList.push(input.value)
+        localStorage.setItem('localItem', JSON.stringify(taskList))
+    }
+    input.value = ""
+    showItems()
 })
+
+function showItems() {
+    let localItems = JSON.parse(localStorage.getItem('localItem'))
+    if(localItems === null){
+        taskList = []
+    } else{
+        taskList = localItems
+    }
+    let UI = ""
+    let taskShow = document.getElementById("header__list")
+    taskList.map((item, index)=> {
+        UI += `
+        <li>
+            <p>${item}</p>
+            <button onclick="deleteItem(${index})">Detele</button>
+        </li>
+        `
+    })
+    taskShow.innerHTML = UI
+}
+showItems()
+
+function deleteItem(index) {
+    taskList.splice(index, 1)
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    window.location.reload()
+}
+// localStorage.clear()
